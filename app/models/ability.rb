a974a7c0945a4367b307e34bf6a5c458e0c4073b
praveen <contact@praveenmuthu.dev>
2019-05-01 15:48:26 +1000

    add auth logic using cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,18 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  def initialize(user)  
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+      user ||= User.new # guest user (not logged in)
+      if user.store.present?
+        can :update, App
+        can :read, App
+        can :create, App
+        can :delete, App, store_id: user.id
+      else
+        can :read, :all
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
