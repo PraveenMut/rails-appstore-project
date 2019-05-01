11976264e421115df679dadc1fa27757843f992b
praveen <contact@praveenmuthu.dev>
2019-05-01 21:42:51 +1000

    updating gitignore

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
   def new
+    @app = App.find(params[:id])
+    @order = Order.new
+    @order.user_id = current_user.id
+    @order.order_price = (@app.price*100).to_i
   end
 
-  def show
+  def create
+
   end
 end
