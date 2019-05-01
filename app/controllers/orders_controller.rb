b1a7045f0927442f3122fb4851b80aaeab7a21e5
praveen <contact@praveenmuthu.dev>
2019-05-01 22:55:00 +1000

    add order controller logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,29 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @app = App.find(params[:id])
+    @amount = (@app.price*100).to_i
+    user = current_user.id
+    @order = Order.new({:user_id=>user,:order_price=>@amount})
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'aud',
+    })
+    if @order.save
+      redirect_to apps_path
+    else
+      @order.errors
+      render 'new'
+    end
+  end
 
+  def show
   end
 end
