a760ef9b76a513d02f047602c78ae1785adefaab
praveen <contact@praveenmuthu.dev>
2019-05-08 14:47:09 +1000

    fix up app edit page

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,6 +2,5 @@ class App < ApplicationRecord
   belongs_to :store
   has_many_attached :images
   has_one_attached :icon
-  validates :Short_Description, presence: true, length: {maximum: 60}, allow_nil: false
   # config.assets.paths << Rails.root.join("app/assets", 'images/home') 
 end
